d90473477fb0db842d7b006497c4b54714f2ee34
Matthias Muenzner <matthias.muenzner@jkaref.com>
2021-01-04 21:25:09 +0100

    add: travis.yml

---
+++ ci/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+#
+
+ set -euxo pipefail
+
+ main() {
+     echo "Preparing rust environment..."
+     if [ "$TARGET" != "x86_64-unknown-linux-gnu" ]; then
+         echo "Installing $TARGET"
+         rustup target add $TARGET
+     fi
+     echo "Environment prepared."
+ }
+
+ main
