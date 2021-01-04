2a1aed36c2abf65c2ea6cbd2e8a0fe0861e60f95
Matthias Muenzner <matthias.muenzner@jkaref.com>
2021-01-04 22:09:14 +0100

    Remove TARGET from ci/install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -6,10 +6,6 @@
 
  main() {
      echo "Preparing rust environment..."
-     if [ "$TARGET" != "x86_64-unknown-linux-gnu" ]; then
-         echo "Installing $TARGET"
-         rustup target add $TARGET
-     fi
      echo "Environment prepared."
  }
 
